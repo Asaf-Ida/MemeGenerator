9aa572baca89d7bb20b3117f1046f60981c1b483
Asaf-Ida <Asafida20@gmail.com>
2024-03-15 16:21:28 +0200

    Create a responsive canvas container for holding an image and text

---
+++ js/meme.controller.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('.editor-meme canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    resizeCanvas()
+    renderMeme()
+    renderMemeText()
+
+    window.addEventListener('resize', () => resizeCanvas())
+}
+
+function renderMeme() {
+    const elImg = new Image()
+    elImg.src = 'img/1.jpg'
+
+    elImg.onload = () => 
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function renderMemeText() {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'orange'
+    gCtx.fillStyle = 'orange'
+    
+    gCtx.font = '45px Arial'
+    
+    gCtx.fillText('I am stupid!', gElCanvas.width / 2, gElCanvas.height / 2)
+    gCtx.strokeText('I am stupid!', gElCanvas.width / 2, gElCanvas.height / 2)
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+
+    gElCanvas.width = elContainer.clientWidth
+}
