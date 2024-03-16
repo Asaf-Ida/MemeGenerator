8d61be0dea04094f06cef277029d849ca051b77c
Asaf-Ida <Asafida20@gmail.com>
2024-03-16 11:35:31 +0200

    Refactor global variable for images

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,12 +1,42 @@
 'use strict'
 
+let gImgs
+
+var gId = 0
+
 let gMeme = {
     lineTxt: 'Add Text Here',
     imgUrl: 'img/19.jpg'
 }
 
+_createImages()
+
+function _createImages() {
+    gImgs = [
+        _createImage(['celeb', 'funny']),
+        _createImage(['dogs', 'cute']),
+        _createImage(['dogs', 'cute', 'babies', 'sleeping']),
+        _createImage(['cats', 'cute', 'sleeping']),
+        _createImage(['babies']),
+        _createImage(['celeb', 'funny'])
+    ]
+}
+
+function _createImage(keywords) {
+    return {
+        id: ++gId, 
+        url: 'img/' + gId + '.jpg',
+        keywords
+    }
+}
+
 function getMeme() {
     return gMeme
+}
+
+function setImg(imgUrlSelected) {
+    console.log(gImgs)
+    gMeme.imgUrl = imgUrlSelected
 }
 
 function setLineTxt(newTxt) {
